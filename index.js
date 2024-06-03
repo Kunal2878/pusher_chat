6a8652cb8152b6915fd24f7b68358b7072301f3a
Kunal <kunalpaul673@gmail.com>
2024-06-04 04:59:04 +0530

    dontenv aded

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 // import Pusher from 'pusher-js';
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const Pusher = require('pusher');
-const pusher = new Pusher({
-  appId:    process.env.PUBLIC_PUSHER_APP_ID,
-  key:      process.env.PUBLIC_PUSHER_KEY,
-  secret:   process.env.PUBLIC_PUSHER_SECRET,
-  cluster:  process.env.PUBLIC_PUSHER_CLUSTER,
-  useTLS: true
-});
 // const pusher = new Pusher({
-//   appId: "1813007",
-//   key: "e01912a32c34c280e6eb",
-//   secret: "6bcb8f5f082d93a4ba8c",
-//   cluster: "ap2",
+//   appId:    process.env.PUBLIC_PUSHER_APP_ID,
+//   key:      process.env.PUBLIC_PUSHER_KEY,
+//   secret:   process.env.PUBLIC_PUSHER_SECRET,
+//   cluster:  process.env.PUBLIC_PUSHER_CLUSTER,
 //   useTLS: true
 // });
+const pusher = new Pusher({
+  appId: "1813007",
+  key: "e01912a32c34c280e6eb",
+  secret: "6bcb8f5f082d93a4ba8c",
+  cluster: "ap2",
+  useTLS: true
+});
 
 pusher.trigger("my-channel", "my-event", {
   message: "hello world"
