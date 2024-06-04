21de96914957182c937753ae20d5f47f99ac444b
Kunal <kunalpaul673@gmail.com>
2024-06-05 01:36:36 +0530

    server modified

--- index.js
+++ index.js
@@ -6,10 +6,29 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 const chat = require("./routes/chat");
-
+const Pusher = require('pusher');
+const pusher = new Pusher({
+  appId: "1813007",
+  key: "e01912a32c34c280e6eb",
+  secret: "6bcb8f5f082d93a4ba8c",
+  cluster: "ap2",
+  useTLS: true
+});
 app.use(cors({ origin: true })); // Adjust origin as needed
 app.use(express.json());
 app.use("/chat", chat);
+app.post("/chat", async (req, res, next) => {
+  const { message } = req.body;
+
+
+  try {
+    await pusher.trigger('chat-channel', 'new-message', { message });
+    res.json({ message: 'Message sent successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Failed to send message' });
+  }
+});
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`,process.env.PUSHER_CLUSTER);
 });
